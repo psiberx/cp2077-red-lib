b25a968893c796d513b6ed902a39d30e47c05b2b
psiberx <psiberx@gmail.com>
2026-03-08 21:22:26 +0300

    Support RED4ext.SDK V1

--- include/RedLib.hpp
+++ include/RedLib.hpp
@@ -11,7 +11,6 @@
 #include <utility>
 #include <vector>
 
-#include <RED4ext/Api/Version.hpp>
 #include <RED4ext/Common.hpp>
 #include <RED4ext/GameEngine.hpp>
 #include <RED4ext/RTTISystem.hpp>
@@ -24,6 +23,12 @@
 #include <RED4ext/Scripting/Natives/ScriptGameInstance.hpp>
 #include <RED4ext/Scripting/OpcodeHandlers.hpp>
 #include <RED4ext/Scripting/Stack.hpp>
+
+#ifdef RED4EXT_OFFSET_TO_ADDR
+#include <RED4ext/Api/v1/Version.hpp>
+#else
+#include <RED4ext/Api/Version.hpp>
+#endif
 
 #include <nameof/nameof.hpp>
 
